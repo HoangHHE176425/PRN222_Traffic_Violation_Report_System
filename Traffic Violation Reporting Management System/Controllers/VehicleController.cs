15bf7e41bf2a98b0865eebda6da9a6e546c3cfb1
ManhLDHE170124 <manhldhe170124@fpt.edu.vn>
2025-07-06 22:17:51 +0700

    vehicle

---
+++ Traffic Violation Reporting Management System/Controllers/VehicleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Traffic_Violation_Reporting_Management_System.Controllers
+{
+    public class VehicleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
