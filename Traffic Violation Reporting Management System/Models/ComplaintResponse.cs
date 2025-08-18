c80df7887c9172304fe1a28c8c0d5f2a6a10ffff
HoangHHE176425 <hoangxmhg2003@gmail.com>
2025-08-18 13:36:06 +0700

    Add ComplaintResponse.cs: Added model to handle complaint responses from authorities

---
+++ Traffic Violation Reporting Management System/Models/ComplaintResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Traffic_Violation_Reporting_Management_System.Models;
+
+public partial class ComplaintResponse
+{
+    public int ResponseId { get; set; }
+
+    public int ComplaintId { get; set; }
+
+    public int ResponderId { get; set; }
+
+    public string? ResponseText { get; set; }
+
+    public DateTime? RespondedAt { get; set; }
+
+    public virtual Complaint Complaint { get; set; } = null!;
+
+    public virtual User Responder { get; set; } = null!;
+}
